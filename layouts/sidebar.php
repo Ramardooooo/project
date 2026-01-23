e5cc5fd6ee759a1ee3014a2118cc7a50634cd821
Ramadhani Fadillah <ramaxkont1@gmail.com>
2026-01-23 14:04:20 +0700

    fix b ug

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -4,8 +4,8 @@
 <li class="px-4 py-2 hover:bg-yellow-700"><a href="home.php">Beranda</a></li>
 <li class="px-4 py-2 hover:bg-yellow-700"><a href="dashboard.php">Dashboard</a></li>
 <?php if ($_SESSION['role'] == 'admin') { ?>
-<li class="px-4 py-2 hover:bg-yellow-700"><a href="users.php">Manage Users</a></li>
+<li class="px-4 py-2 hover:bg-yellow-700"><a href="../users.php">Manage Users</a></li>
 <?php } ?>
-<li class="px-4 py-2 hover:bg-yellow-700"><a href="../auth/logout.php">Logout</a></li>
+<li class="px-4 py-2 hover:bg-yellow-700"><a href="rama.php">Ramadhani Fadillah</a></li>
 </ul>
 </div>
