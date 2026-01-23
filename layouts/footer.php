ffd8f9f09b89a0924b5a0a1c73fd46f50900ec60
Ramadhani Fadillah <ramaxkont1@gmail.com>
2026-01-24 04:38:09 +0700

    Delete layouts/footer.php

--- layouts/footer.php
+++
@@ -1,8 +0,0 @@
-<?php include '../layouts/footer.php'; ?>
-<footer class="bg-yellow-600 text-white py-8">
-    <div class="container mx-auto px-4 text-center">
-        <p>&copy; 2025 Lurahgo.id - Sistem Informasi RT/RW</p>
-        <p>Contact Admin: admin@lurago.id</p>
-    </div>
-</footer>
-</body></html>
