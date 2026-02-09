533697fc2ca53a7e34083812fe3ebec8e438c9be
Ramadhani Fadillah <ramaxkont1@gmail.com>
2026-02-09 21:23:04 +0700

    Delete layouts directory

--- layouts/user/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="w-64 bg-gradient-to-b from-slate-600 to-slate-700 text-slate-100 min-h-screen fixed">
-<div class="p-4 text-xl font-bold">Lurago.id</div>
-<ul class="mt-4">
-<li class="px-4 py-2 hover:bg-yellow-700"><a href="/PROJECT/home">Beranda</a></li>
-<li class="px-4 py-2 hover:bg-slate-600"><a href="/PROJECT/dashboard_user">Dashboard</a></li>
-<li class="px-4 py-2 hover:bg-slate-600"><a href="/PROJECT/profile">Profile</a></li>
-</ul>
-</div>
